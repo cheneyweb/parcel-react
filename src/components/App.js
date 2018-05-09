bc481dbbc18361e265606fc626b4c3be642009e6
cheneyweb <457299596@qq.com>
2018-05-09 15:49:12 +0800

    样式完善

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,7 @@ export default class App extends React.Component {
                 <div className="logo" />
                 <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
                     <Menu.Item key="1">
-                        <Link to="/ciflow">
+                        <Link to="/">
                             <Icon type="table" />
                             <span className="nav-text">构建服务流</span>
                         </Link>
@@ -25,20 +25,20 @@ export default class App extends React.Component {
                             <span className="nav-text">构建配置</span>
                         </Link>
                     </Menu.Item>
-                    <Menu.Item key="4">
+                    {/* <Menu.Item key="4">
                         <Link to="/nostate">
                             <Icon type="nostate" />
                             <span className="nav-text">无状态组件</span>
                         </Link>
-                    </Menu.Item>
+                    </Menu.Item> */}
                 </Menu>
             </Sider>
             <Layout style={{ marginLeft: 200 }}>
                 {/* <Header style={{ background: '#fff', padding: 0 }} /> */}
                 <Content style={{ margin: '24px 16px 70px', overflow: 'initial' }}>
+                    <Route exact path="/" component={CIFlow} />
                     <Route path="/ciconfig" component={CIConfig} />
-                    <Route path="/ciflow" component={CIFlow} />
-                    <Route path="/nostate" component={NoState} />
+                    {/* <Route path="/nostate" component={NoState} /> */}
                 </Content>
                 <Footer style={{ position: 'fixed', bottom: 0, height: '70px', width: '100%', textAlign: 'center' }}>
                     XServer Design ©2018 Created by CheneyXu
